51f105866f9ecf05edb51ab1a0d472222cca4120
matthias <matthias.fuchs>
2023-01-04 12:36:23 +0100

    change time

--- src/utilities/checkLaneData.tsx
+++ src/utilities/checkLaneData.tsx
@@ -1,4 +1,6 @@
 import { LaneState } from "../state/LaneState";
+import getBirthYear from "./getBirthYear";
+import getEntryTime from "./getEntryTime";
 
 function getIsLap(place: any): boolean {
 
@@ -9,22 +11,58 @@ function getIsLap(place: any): boolean {
     }
 }
 
-export function correctItem(jsondata: any): LaneState {
+function getChangedate(KeyJsomlanes: string, Keyjsondata: string, oldchangeDate: number): number {
+
+    if (KeyJsomlanes === Keyjsondata) {
+        return oldchangeDate
+    } else {
+        console.log('change')
+        return Date.now()
+    }
+
+}
+
+export function correctItem(jsondata: any, Jsonlane: LaneState): LaneState {
+
+    var oldChange = Jsonlane === undefined ? Date.now() : Jsonlane.changed
+    var KeyJsomlanes = Jsonlane === undefined ? '' : Jsonlane.lane + Jsonlane.finishtime + Jsonlane.swimmerData.name + Jsonlane.place
+    var Keyjsondata = jsondata === undefined ? '' : jsondata.lane + jsondata.finishtime + jsondata.lastname + jsondata.place
+
+    var newDate = getChangedate(KeyJsomlanes, Keyjsondata, oldChange)
 
     let laneState: LaneState =
     {
-        changed: 0,
+        changed: newDate,
         finishtime: jsondata.finishtime,
         islaptime: getIsLap(jsondata.place),
         lane: jsondata.lane,
-        laptime: Date.now().toString(),
+        laptime: "",
         place: jsondata.place,
+        entrytime: getEntryTime(jsondata.entrytime),
         swimmerData: {
             clubid: jsondata.code,
             clubname: jsondata.name,
             name: jsondata.lastname,
+            birthyear: getBirthYear(jsondata.birthdate),
+            firstName: jsondata.firstname,
         },
     }
     return laneState
+}
 
+export function correctDisplaymode(jsondata: any, DisplayMode: string): string {
+    if (jsondata.place === '0') {
+        return 'race'
+    } else if (jsondata.finishtime === "undefined" || !jsondata.finishtime) {
+        if (DisplayMode !== 'startlist' && DisplayMode !== 'race') {
+            return 'startlist'
+        } else {
+            return DisplayMode
+        }
+    } else {
+        if (DisplayMode !== 'race') {
+            return 'race'
+        }
+        return DisplayMode
+    }
 }
